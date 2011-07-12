81468b199336f7d6e481c458cf33321b8b473658
Dominik Gehl <dominik@dokdok.com>
2011-07-12 16:07:00 -0400

    adapted examples

--- examples.php
+++ examples.php
@@ -2,60 +2,64 @@
 <?php
 // remove first line above if you're not running these examples through PHP CLI
 
-
 include_once("class.contextio.php");
 
 // see https://console.context.io/#settings to get your consumer key and consumer secret.
-
 $contextIO = new ContextIO('consumerKeyHere','consumerSecretHere');
+$accountId = null;
 
-// This is optional
-$contextIO->useAuthorizationHeaders(true);
+// list your accounts
+$r = $contextIO->listAccounts();
+foreach ($r->getData() as $account) {
+	echo $account['id'] . "\t" . join(", ", $account['email_addresses']) . "\n";
+	if (is_null($accountId)) {
+		$accountId = $account['id'];
+	}
+}
 
-// Specify the mailbox we'll get data from. That mailbox must be added to your
-// API key through https://console.context.io or ContextIO->imap_addAccount()
-$mailboxToQuery = 'jim@acme.com';
-
+if (is_null($accountId)) {
+	die;
+}
 
 // EXAMPLE 1
 // Print the subject line of the last 20 emails sent to with bill@widgets.com
 $args = array('to'=>'bill@widgets.com', 'limit'=>20);
 echo "\nGetting last 20 messages exchanged with {$args['to']}\n";
-$r = $contextIO->contactMessages($mailboxToQuery, $args);
+$r = $contextIO->listMessages($accountId, $args);
 foreach ($r->getData() as $message) {
 	echo "Subject: ".$message['subject']."\n";
 }
 
 // EXAMPLE 2
 // Download all versions of the last 2 attachments exchanged with bill@widgets.com
-
 $saveToDir = dirname(__FILE__)."/".mt_rand(100,999);
 mkdir($saveToDir);
 
 $args = array('email'=>'bill@widgets.com', 'limit'=>2);
-$r = $contextIO->contactFiles($mailboxToQuery, $args);
+echo "\nObtaining list of last two attachments exchanged with {$args['email']}\n";
+$r = $contextIO->listFiles($accountId, $args);
 foreach ($r->getData() as $document) {
-	echo "\nDownloading all versions of document \"".$document['fileName']."\"\n";
+	echo "\nDownloading all versions of document \"".$document['file_name']."\"\n";
 	foreach ($document['occurrences'] as $attachment) {
-		echo "Downloading attachment '".$attachment['fileName']."' to $saveToDir ... ";
-		$contextIO->downloadFile($mailboxToQuery, array('fileId'=>$attachment['fileId']), $saveToDir."/".$attachment['fileName']);
+		echo "Downloading attachment '".$attachment['file_name']."' to $saveToDir ... ";
+		$contextIO->getFileContent($accountId, array('file_id'=>$attachment['fileId']), $saveToDir."/".$attachment['file_name']);
 		echo "done\n";
 	}
 }
 
 // EXAMPLE 3
 // Download all attachments with a file name that contains the word 'proposal'
-
 $saveToDir = dirname(__FILE__)."/".mt_rand(100,999);
 mkdir($saveToDir);
 
 echo "\nDownloading all attachments matching 'proposal'\n";
-$args = array('filename'=>'proposal');
-$r = $contextIO->fileSearch($mailboxToQuery, $args);
+$args = array('name'=>'creenshot');
+$r = $contextIO->listFiles($accountId, $args);
 foreach ($r->getData() as $attachment) {
-	echo "Downloading attachment '".$attachment['fileName']."' to $saveToDir ... ";
-	$contextIO->downloadFile($mailboxToQuery, array('fileId'=>$attachment['fileId']), $saveToDir."/".$attachment['fileName']);
+	echo "Downloading attachment '".$attachment['file_name']."' to $saveToDir ... ";
+	$contextIO->getFileContent($accountId, array('file_id'=>$attachment['file_id']), $saveToDir."/".$attachment['file_name']);
 	echo "done\n";
 }
 
+echo "\nall examples finished\n";
 ?>
