63590c8ad3030e0dffc729f2bd4cdccbf89e82d3
Dominik Gehl <dominik@dokdok.com>
2011-07-27 14:48:35 -0400

    changed addSource call parameters

--- class.contextio.php
+++ class.contextio.php
@@ -854,7 +854,7 @@ class ContextIO {
 		if (is_null($account) || ! is_string($account) || (! strpos($account, '@') === false)) {
 			throw new InvalidArgumentException('account must be string representing accountId');
 		}
-		$params = $this->_filterParams($params, array('type','email','server','username','oauth_consumer_key','oauth_token','oauth_token_secret','service_level','password','use_ssl','port'), array('server','username'));
+		$params = $this->_filterParams($params, array('type','email','server','username','provider_consumer_key','provider_token','provider_token_secret','service_level','password','use_ssl','port'), array('server','username'));
 		if ($params === false) {
 			throw new InvalidArgumentException("params array contains invalid parameters or misses required parameters");
 		}
